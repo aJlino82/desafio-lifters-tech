5975ecac64cdfff0c3622d0a2bb611cc9056d63d
Adailton Junior <adailton.jr@gmail.com>
2024-03-20 09:45:56 -0300

    primeiro commit

---
+++ src/test/java/desafio/lifters/tech/LiftersApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.lifters.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
